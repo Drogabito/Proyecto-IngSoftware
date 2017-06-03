f42cc995d998a5435e5712df28baa3af620a0455
Rodrigo Elicer <rodrigo.elicer1@gmail.com>
2017-06-02 22:10:44 -0400

    start web scrawling

--- Quantum/src/app/ver/ver.component.ts
+++ Quantum/src/app/ver/ver.component.ts
@@ -4,9 +4,9 @@ import { Component, OnInit } from '@angular/core';
 declare var jQuery:any;
 declare var $:any;
 
-const {cheerio} = require('cheerio');
-//request está causando problemas!!!!
+let cheerio = require('cheerio');
 let request = require('request');
+let URL = require('url-parse');
 
 @Component({
   selector: 'app-ver',
@@ -14,24 +14,58 @@ let request = require('request');
 })
 export class VerComponent implements OnInit{
 
-	urls = [];
 	title = 'Probando jQuery';
+	pageToVisit = "http://www.arstechnica.com";
 
   	ngOnInit() {}
 
 	scraping(){
-		request('https://news.ycombinator.com', function (error, response, html) {
-		  	if (!error && response.statusCode == 200) {
-		    	console.log(html);
-		  	}
-		});
+		console.log("Visiting page " + this.pageToVisit);
+		request(this.pageToVisit, function(error, response, body) {
+		   if(error) {
+		     console.log("Error: " + error);
+		   }
+		   // Check status code (200 is HTTP OK)
+		   console.log("Status code: " + response.statusCode);
+		   if(response.statusCode === 200) {
+		     // Parse the document body
+		     $ = cheerio.load(body);
+		     console.log("Page title:  " + $('title').text());
+			 collectLinks($);
+		   }
 
-		console.log('scraping!');
+		   function collectLinks($) {
+		     var allRelativeLinks = [];
+		     var allAbsoluteLinks = [];
+
+		     var relativeLinks = $("a[href^='/']");
+		     relativeLinks.each(function() {
+		   	  allRelativeLinks.push($(this).attr('href'));
+
+		     });
+
+		     var absoluteLinks = $("a[href^='http']");
+		     absoluteLinks.each(function() {
+		   	  allAbsoluteLinks.push($(this).attr('href'));
+		     });
+
+		     console.log("Found " + allRelativeLinks.length + " relative links");
+		     console.log("Found " + allAbsoluteLinks.length + " absolute links");
+		     return;
+		 	}
+		});
+	}
+
+	searchForWord($, word) {
+	  var bodyText = $('html > body').text();
+	  if(bodyText.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
+	    return true;
+	  }
+	  return false;
 	}
 
 	public mostrarTitulo(){
 		console.log("click");
-
 	   	//jQuery
 	    $(".title").slideToggle();
   	}
