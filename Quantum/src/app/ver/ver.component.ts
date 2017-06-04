d0de8e528aab9f20e9d653cf199ba5145d2421e4
Rodrigo Elicer <rodrigo.elicer1@gmail.com>
2017-06-03 23:22:18 -0400

    cleaning code

--- Quantum/src/app/ver/ver.component.ts
+++ Quantum/src/app/ver/ver.component.ts
@@ -1,21 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
-//jQuery
-//declare var jQuery:any;
-//declare var $:any;
-
 let cheerio = require('cheerio');
 let request = require('request');
-//let URL = require('url-parse');
 
 @Component({
   selector: 'app-ver',
   templateUrl: './ver.component.html'
 })
 export class VerComponent implements OnInit{
-
-	//title = 'Probando jQuery';
-	//pageToVisit = "http://www.arstechnica.com";
 
 	START_URL = "http://www.arstechnica.com";
 	MAX_PAGES_TO_VISIT = 3;
@@ -28,7 +20,7 @@ export class VerComponent implements OnInit{
 		this.pagesToVisit.push(this.START_URL);
 	}
 
-	crawl(){
+	scraping(){
 		if(this.numPagesVisited>=this.MAX_PAGES_TO_VISIT){
 			console.log("Reached max limit of number of pages to visit.");
 		    return;
@@ -36,7 +28,7 @@ export class VerComponent implements OnInit{
 		var nextPage = this.pagesToVisit.pop();
 		if(nextPage in this.pagesVisited){
 			//Ya se visitó, por ende se sigue buscando
-			this.crawl();
+			this.scraping();
 		}
 		else{
 			//Se visita la página con cheerio (scraping)
@@ -59,7 +51,7 @@ export class VerComponent implements OnInit{
 			if(response.statusCode === 200){
 				var cherry = cheerio.load(body);
 				self.collectAbsoluteLinks(cherry);
-				self.crawl();
+				self.scraping();
 			}
 
 		});
@@ -74,43 +66,5 @@ export class VerComponent implements OnInit{
 		});
 		console.log(self.pagesToVisit.length + " pagesToVisit");
 	}
-
-	/*scraping(){
-		console.log("Visiting page " + this.pageToVisit);
-		const self=this;
-		request(this.pageToVisit, function(error, response, body:string) {
-		   	if(error) {
-		     	console.log("Error: " + error);
-		   	}
-		   	// Check status code (200 is HTTP OK)
-		   	console.log("Status code: " + response.statusCode);
-		   	if(response.statusCode === 200) {
-		    	// Parse the document body
-		     	var cherry = cheerio.load(body);
-		     	console.log("Page title:  " + $('title').text());
-			 	self.collectLinks(cherry);
-		   	}
- 		});
-	}*/
-
-	/*collectLinks(cherry) {
-		var allRelativeLinks = [];
-		var allAbsoluteLinks = [];
-
-		var relativeLinks = cherry("a[href^='/']");
-		relativeLinks.each(function() {
-		 	allRelativeLinks.push(cherry(this).attr('href'));
-		});
-
-		var absoluteLinks = cherry("a[href^='http']");
-		absoluteLinks.each(function() {
-		 	allAbsoluteLinks.push(cherry(this).attr('href'));
-		});
-
-		console.log("Found " + allRelativeLinks.length + " relative links");
-		console.log(allRelativeLinks);
-		console.log("Found " + allAbsoluteLinks.length + " absolute links");
-		console.log(allAbsoluteLinks);
-	}*/
 
 }
