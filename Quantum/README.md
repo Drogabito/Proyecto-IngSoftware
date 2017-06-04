2741978e53ea64f22421ac1374209ffab960f4ca
Rodrigo Elicer <rodrigo.elicer1@gmail.com>
2017-06-03 23:10:29 -0400

    scrapeando X sitios, obteniendo links correctamente

---
+++ Quantum/README.md
@@ -0,0 +1,59 @@
+## Quick start
+
+```bash
+https://github.com/GabrielValenzuelaLorca/Proyecto-IngSoftware.git
+```
+Instalar dependencias
+
+```bash
+cd Proyecto-IngSoftware/Quantum
+npm install
+```
+Ejecutar la App
+```bash
+npm start
+```
+
+## Packaging(For test)
+
+The app has support for packaging using 'electron-packager'
+
+```bash
+$ npm run package -- --all
+```
+
+Will run the package for OSX. You can also provide additional options to the package command such as
+
+*  --name : The package name
+*  --all : Will packaget the application to all the platforms
+*  --arch : Arches to be provided
+*  --icon : The icon for the app
+
+## Generate Installer
+
+Modify [electron-builder.yml](./electron-builder.yml) to edit package info.
+
+For a full list of options see: https://github.com/electron-userland/electron-builder/wiki/Options.
+
+Create a package for OSX, Windows and Linux
+```
+npm run pack
+```
+
+Or target a specific platform
+```
+npm run pack:mac
+npm run pack:win
+npm run pack:linux
+```
+
+## License
+
+[MIT]
+
+[Webpack]: http://webpack.github.io
+[MIT]: http://markdalgleish.mit-license.org
+[angular2]: http://angular.io
+[electron]: http://electron.atom.io/
+[ngrx]: https://github.com/ngrx/store
+[electron-packager]: https://github.com/electron-userland/electron-packager
--- Quantum/src/app/ver/ver.component.ts
+++ Quantum/src/app/ver/ver.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
 
 let cheerio = require('cheerio');
 let request = require('request');
-let URL = require('url-parse');
+//let URL = require('url-parse');
 
 @Component({
   selector: 'app-ver',
@@ -14,12 +14,68 @@ let URL = require('url-parse');
 })
 export class VerComponent implements OnInit{
 
-	title = 'Probando jQuery';
-	pageToVisit = "http://www.arstechnica.com";
+	//title = 'Probando jQuery';
+	//pageToVisit = "http://www.arstechnica.com";
 
-  	ngOnInit() {}
+	START_URL = "http://www.arstechnica.com";
+	MAX_PAGES_TO_VISIT = 3;
 
-	scraping(){
+	pagesVisited = {};
+	pagesToVisit = [];
+	numPagesVisited = 0;
+
+  	ngOnInit() {
+		this.pagesToVisit.push(this.START_URL);
+	}
+
+	crawl(){
+		if(this.numPagesVisited>=this.MAX_PAGES_TO_VISIT){
+			console.log("Reached max limit of number of pages to visit.");
+		    return;
+		}
+		var nextPage = this.pagesToVisit.pop();
+		if(nextPage in this.pagesVisited){
+			//Ya se visitó, por ende se sigue buscando
+			this.crawl();
+		}
+		else{
+			//Se visita la página con cheerio (scraping)
+			this.visitPage(nextPage);
+		}
+	}
+
+	visitPage(url){
+		this.pagesVisited[url] = true;
+		this.numPagesVisited++;
+
+		console.log("Visiting page " + url);
+		const self=this;
+
+		request(url,function(error,response,body:string){
+			if(error){
+				console.log("Error: " + error);
+			}
+			console.log("Status code: " + response.statusCode);
+			if(response.statusCode === 200){
+				var cherry = cheerio.load(body);
+				self.collectAbsoluteLinks(cherry);
+				self.crawl();
+			}
+
+		});
+	}
+
+	collectAbsoluteLinks(cherry){
+		var absoluteLinks = cherry("a[href^='http']");
+
+		const self=this;
+		absoluteLinks.each(function() {
+		 	self.pagesToVisit.push(cherry(this).attr('href'));
+		});
+		console.log(self.pagesToVisit.length + " pagesToVisit");
+	}
+
+	/*scraping(){
 		console.log("Visiting page " + this.pageToVisit);
 		const self=this;
 		request(this.pageToVisit, function(error, response, body:string) {
@@ -35,24 +91,26 @@ export class VerComponent implements OnInit{
 			 	self.collectLinks(cherry);
 		   	}
  		});
-	}
+	}*/
 
-	collectLinks(cherry) {
-		 var allRelativeLinks = [];
-		 var allAbsoluteLinks = [];
+	/*collectLinks(cherry) {
+		var allRelativeLinks = [];
+		var allAbsoluteLinks = [];
 
-		 var relativeLinks = cherry("a[href^='/']");
-		 relativeLinks.each(function() {
+		var relativeLinks = cherry("a[href^='/']");
+		relativeLinks.each(function() {
 		 	allRelativeLinks.push(cherry(this).attr('href'));
-		 });
+		});
 
-		 var absoluteLinks = cherry("a[href^='http']");
-		 absoluteLinks.each(function() {
-		 allAbsoluteLinks.push(cherry(this).attr('href'));
-		 });
+		var absoluteLinks = cherry("a[href^='http']");
+		absoluteLinks.each(function() {
+		 	allAbsoluteLinks.push(cherry(this).attr('href'));
+		});
 
-		 console.log("Found " + allRelativeLinks.length + " relative links");
-		 console.log("Found " + allAbsoluteLinks.length + " absolute links");
-	 }
+		console.log("Found " + allRelativeLinks.length + " relative links");
+		console.log(allRelativeLinks);
+		console.log("Found " + allAbsoluteLinks.length + " absolute links");
+		console.log(allAbsoluteLinks);
+	}*/
 
 }
