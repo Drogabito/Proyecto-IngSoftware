4204819173854dbe79b0f09c58fb76699d887056
Rodrigo Elicer <rodrigo.elicer1@gmail.com>
2017-06-10 19:30:29 -0400

    deleted prod

--- Quantum/webpack.config.js
+++ Quantum/webpack.config.js
@@ -1,8 +1,4 @@
 switch (process.env.NODE_ENV) {
-  	case 'prod':
-  	case 'production':
-    	module.exports = require('./config/webpack.prod')({env: 'production'});
-    	break;
   	case 'dev':
   	case 'development':
   	default:
